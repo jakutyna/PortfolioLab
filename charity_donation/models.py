bd4905ca06afbe06d632926837c2b517bce265d4
Jacek <jakutyna@gmail.com>
2020-11-11 16:03:32 +0100

    Add models

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class Institution(models.Model):
+    FOUNDATION = 1
+    NGO = 2
+    LOCAL = 3
+
+    TYPE_CHOICES = (
+        (FOUNDATION, "fundacja"),
+        (NGO, "organizacja pozarządowa"),
+        (LOCAL, "zbiórka lokalna"),
+    )
+
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE_CHOICES, default=FOUNDATION)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    address = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=64)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
